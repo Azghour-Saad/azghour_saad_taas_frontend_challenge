2f52a9885e51da1f375c984cd9ca95cdc0048a71
Azghour-Saad <saad.azghour@gmail.com>
2022-03-23 03:11:51 +0000

    Add, AuthorizeBtn comp

---
+++ src/components/AuthorizeBtn.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="auth">
+    <!-- <h1>{{ msg }}</h1> -->
+    <button
+      class="justify-center px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
+    >
+      Authorize my Github account
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthorizeBtn",
+  props: {
+    // msg: String
+  },
+
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
