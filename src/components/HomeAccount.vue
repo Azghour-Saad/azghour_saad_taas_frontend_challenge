d1c7e0770d8fd61bafc64fe0a0c5adf0726df51f
Azghour-Saad <saad.azghour@gmail.com>
2022-03-25 19:50:14 +0000

    Add, filter by name functionality!

--- src/components/HomeAccount.vue
+++ src/components/HomeAccount.vue
@@ -1,38 +1,63 @@
 <template>
-  <Menu as="div" class="relative inline-block text-left">
-    <div>
-      <MenuButton
-        class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
-      >
-        name
-        <ChevronDownIcon class="w-5 h-5 ml-2 -mr-1" aria-hidden="true" />
-      </MenuButton>
-    </div>
-
-    <transition
-      enter-active-class="transition duration-100 ease-out"
-      enter-from-class="transform scale-95 opacity-0"
-      enter-to-class="transform scale-100 opacity-100"
-      leave-active-class="transition duration-75 ease-in"
-      leave-from-class="transform scale-100 opacity-100"
-      leave-to-class="transform scale-95 opacity-0"
+  <div class="flex flex-wrap space-x-4">
+    <div
+      class="flex items-center justify-start ml-4 font-sans text-black bg-white"
     >
-      <MenuItems
-        class="absolute right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-      >
-        <div class="py-1" v-for="item in getRepo" :key="item.id">
-          <MenuItem v-slot="{ active }">
-            <a
-              href="#"
-              :class="[
-                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                'block px-4 py-2 text-sm',
-              ]"
-              >{{ item.name }}</a
-            >
-          </MenuItem>
+      <div class="flex overflow-hidden border rounded">
+        <input
+          type="text"
+          class="px-4 py-2"
+          placeholder="Start searching by name..."
+          v-model="search"
+        />
+        <button class="flex items-center justify-center px-4 border-l">
+          <svg
+            class="w-4 h-4 text-grey-dark"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+    <Menu as="div" class="relative inline-block text-left">
+      <div>
+        <MenuButton
+          class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+        >
+          name
+          <ChevronDownIcon class="w-5 h-5 ml-2 -mr-1" aria-hidden="true" />
+        </MenuButton>
+      </div>
 
-          <!-- <form method="POST" action="#">
+      <transition
+        enter-active-class="transition duration-100 ease-out"
+        enter-from-class="transform scale-95 opacity-0"
+        enter-to-class="transform scale-100 opacity-100"
+        leave-active-class="transition duration-75 ease-in"
+        leave-from-class="transform scale-100 opacity-100"
+        leave-to-class="transform scale-95 opacity-0"
+      >
+        <MenuItems
+          class="absolute right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+        >
+          <div class="py-1" v-for="item in filterByName" :key="item.id">
+            <MenuItem v-slot="{ active }">
+              <a
+                href="#"
+                :class="[
+                  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                  'block px-4 py-2 text-sm',
+                ]"
+                >{{ item.name }}</a
+              >
+            </MenuItem>
+
+            <!-- <form method="POST" action="#">
             <MenuItem v-slot="{ active }">
               <button
                 type="submit"
@@ -45,10 +70,11 @@
               </button>
             </MenuItem>
           </form> -->
-        </div>
-      </MenuItems>
-    </transition>
-  </Menu>
+          </div>
+        </MenuItems>
+      </transition>
+    </Menu>
+  </div>
 </template>
 
 <script>
@@ -78,8 +104,9 @@ export default {
   data() {
     return {
       config,
-      code: this.$route.query.code,
+      search: "",
       getRepo: data,
+      code: this.$route.query.code,
     };
   },
 
@@ -87,6 +114,15 @@ export default {
     // console.log("Mounted Code!!", this.$route.query);
     // this.requestGithubToken();
     // this.getRepoUser();
+  },
+  computed: {
+    filterByName: function () {
+      return this.getRepo.filter((repo) => {
+        if (repo.name !== null) {
+          return repo.name.toLowerCase().includes(this.search.toLowerCase());
+        }
+      });
+    },
   },
 
   methods: {
