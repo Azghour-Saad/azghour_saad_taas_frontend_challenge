85e3c2461552c67cd36f752289f8ce41a047e8ff
Azghour-Saad <saad.azghour@gmail.com>
2022-03-26 16:12:40 +0000

    Add CommitHistory Component

---
+++ src/components/CommitHistory.vue
@@ -0,0 +1,22 @@
+<template lang="">
+  <div>
+    <article>
+      <h3>commit Message: {{ commitMessage }}</h3>
+      <p>Author Of Commit: {{ authorOfCommit }}</p>
+      <!-- <p>{{ commitHash }}</p> -->
+      <p>Date Of Commit: {{ dateOfCommit }}</p>
+    </article>
+  </div>
+</template>
+<script>
+export default {
+  name: "CommitHistory",
+  props: {
+    commitMessage: String,
+    hash: String,
+    dateOfCommit: String,
+    authorOfCommit: String,
+  },
+};
+</script>
+<style lang=""></style>
