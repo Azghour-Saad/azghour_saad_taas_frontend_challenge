edc0db915552bdeda1922813e0f347007b32788e
Azghour-Saad <saad.azghour@gmail.com>
2022-03-27 21:25:57 +0000

    feat: Shows the commit history

--- src/components/CommitHistory.vue
+++ src/components/CommitHistory.vue
@@ -1,11 +1,24 @@
 <template lang="">
   <div>
-    <article>
-      <h3>commit Message: {{ commitMessage }}</h3>
-      <p>Author Of Commit: {{ authorOfCommit }}</p>
-      <!-- <p>{{ commitHash }}</p> -->
-      <p>Date Of Commit: {{ dateOfCommit }}</p>
-    </article>
+    <div
+      class="mt-0 space-x-2 text-base border-2 border-solid rounded border-slate-400 bg-gray-50"
+    >
+      <div class="flex items-start mt-1 ml-2">
+        <h4 class="font-semibold text-slate-500">{{ commitMessage }}</h4>
+      </div>
+      <div class="flex items-center">
+        <div>
+          <img
+            class="inline-block w-12 h-12 mt-2 mb-2 mr-2 rounded-full ring-2 ring-white"
+            :src="authorAvatar"
+          />
+        </div>
+        <div>
+          <strong class="mr-1 text-xs">{{ authorOfCommit }}</strong>
+          <span class="text-xs">{{ dateOfCommit }}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -16,6 +29,7 @@ export default {
     hash: String,
     dateOfCommit: String,
     authorOfCommit: String,
+    authorAvatar: String,
   },
 };
 </script>
