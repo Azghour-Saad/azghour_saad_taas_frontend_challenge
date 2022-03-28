3313948c7ca3a5514a9174b5cf24c643d5f5f4af
Azghour-Saad <saad.azghour@gmail.com>
2022-03-28 20:33:03 +0000

    feat: Add, Infinite scroll
    (Need some FIX, i mean by that's scrolling without click again in Dropdown

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue"
 import router from './router/router'
 import "./assets/css/tailwind.css"
 import moment from "moment"
+import VueObserveVisibility from "vue3-observe-visibility";
 
-createApp(App).use(router).use(moment).mount("#app")
+createApp(App).use(router).use(moment).use(VueObserveVisibility).mount("#app");
 
