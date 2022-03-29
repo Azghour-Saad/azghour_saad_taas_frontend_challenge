57a94b8e21f174d363ba7b82c026229919afd10e
Azghour-Saad <saad.azghour@gmail.com>
2022-03-29 00:42:33 +0000

    Use Github API to get Data(Commit)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from './router/router'
 import "./assets/css/tailwind.css"
+import moment from "moment"
 
-createApp(App).use(router).mount("#app")
+createApp(App).use(router).use(moment).mount("#app")
 
